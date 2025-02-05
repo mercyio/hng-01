ff9c9d6c0bce8fbf766eaf07082548dc1ab00687
mercy vincent <mercydanke@gmail.com>
2025-02-05 15:36:07 +0100

    updated envaroment variables

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ENVIRONMENT } from './common/configs/environment';
 
+const serverPort = ENVIRONMENT.APP.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
@@ -12,6 +14,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api');
 
-  await app.listen(5575);
+  await app.listen(serverPort);
 }
-bootstrap();
+bootstrap().then(() =>
+  console.log(
+    `======= SERVER STARTED SUCCESSFULLY ON PORT : ${serverPort} ========`,
+  ),
+);
